f5f3f57db340a7c3006d70a86dd7924730e06686
Chandrakala <44455401+ChandrakalaRatan@users.noreply.github.com>
2023-08-04 22:06:44 -0400

    initial commit
    add the code

---
+++ index.d.ts
@@ -0,0 +1,38 @@
+declare module '@material-ui/pickers';
+declare module 'react-widgets';
+declare module 'react-widgets-moment';
+declare module '@trendmicro/react-dropdown';
+declare module 'qs';
+declare module 'socket.io-client';
+declare module 'react-big-calendar';
+declare module 'chartjs-plugin-piechart-outlabels';
+declare module 'react-reveal';
+declare module 'react-dual-listbox';
+declare module 'react-timer-mixin';
+declare module 'react-html5-camera-photo';
+declare module 'mammoth';
+declare module 'react-pdf';
+declare module 'react-pdf/dist/entry.webpack';
+declare module 'd3';
+declare module 'dagre-d3';
+declare module 'react-privottable/PivotTableUI';
+declare module 'react-pivottable/TableRenderers';
+declare module 'react-pivottable/PlotlyRenderers';
+declare module 'react-plotly.js';
+declare module '@material-ui/core';
+declare module 'react-csv';
+declare module 'jspdf';
+declare module 'qrcode';
+declare module 'jimp';
+declare module 'react-copy-to-clipboard';
+declare module 'rtf.js' {
+    export class DocumentParser {
+      parseString(rtf: string): Promise<Document>;
+    }
+  
+    export class Document {
+      content: {
+        toHtml(): string;
+      };
+    }
+  }
