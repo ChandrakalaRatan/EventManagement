2d48e987895a2356b078a4e42b2717e555ad95b2
Chandrakala <44455401+ChandrakalaRatan@users.noreply.github.com>
2023-08-04 22:14:17 -0400

    src files upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import EventMangementHome from './ManageEvents/EventMangementHome';
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+  return (
+    <div>
+      <EventMangementHome />
+    </div>
+  );
+};
+export default App;
+
